c16c717341137a849f0729bb73c8f94623180156
Anderson Ricardo Palezi <andersonhagna@hotmail.com>
2023-12-12 09:56:58 -0300

    att

--- script/script.js
+++ script/script.js
@@ -1,13 +1,1 @@
-let btnMenu = document.getElementById("btn-menu")
-let menu= document.getElementById("menu-mobile")
-let overlay = document.getElementById("overlay-menu")
-
-btnMenu.addEventListener("click",()=>{
-    menu.classList.add("abrir-menu")
-})
-menu.addEventListener("click",()=>{
-    menu.classList.remove("abrir-menu")
-})
-overlay.addEventListener("click",()=>{
-    menu.classList.remove("abrir-menu")
-})
+let btnMenu=document.getElementById("btn-menu"),menu=document.getElementById("menu-mobile"),overlay=document.getElementById("overlay-menu");btnMenu.addEventListener("click",()=>{menu.classList.add("abrir-menu")}),menu.addEventListener("click",()=>{menu.classList.remove("abrir-menu")}),overlay.addEventListener("click",()=>{menu.classList.remove("abrir-menu")});
